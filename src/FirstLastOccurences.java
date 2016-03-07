44576daa5437e7264b158befd164408f31d07e93
Tet Contado <delrio.tet@gmail.com>
2016-03-06 19:12:57 -0800

    re-arrange file organization

--- src/FirstLastOccurences.java
+++ src/FirstLastOccurences.java
@@ -1,6 +1,27 @@
 
 public class FirstLastOccurences {
-	int[] arr = {1,2,3,4,5,6,6,6,7,7,8,8,8,9};
+	public static void findOccurrences(int[] arr, int given) {
+		int first = -1;
+		int last = -1;
+		int counter = 0;
+		for (int i=0; i<arr.length; i++) {
+			if (arr[i] == given) {
+				first = i;
+				continue;
+			}
+			if (first != -1 && arr[i] == arr[first]) {
+				counter++;
+				last = counter;
+			}
+		}
+		System.out.println(first + ", " + last);
+	}
+	
+	
+	public static void main(String[] args) {
+		int[] arr = {1,1,1,2,3};
+		findOccurrences(arr, 6);
+	}
 	
 	
 }
